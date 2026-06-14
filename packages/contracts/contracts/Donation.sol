0b06e002fa67f35077394b14f860c64cfd2e7564
mbraun6 <marius.braun@hsbi.de>
2026-06-14 18:20:10 +0200

    added milestones and simple voting to the donation contract. milestone logic is not yet sufficient.

--- packages/contracts/contracts/Donation.sol
+++ packages/contracts/contracts/Donation.sol
@@ -3,10 +3,32 @@ pragma solidity ^0.8.28;
 
 contract Donation{
 
+    address public contractOwner;
     uint256 public donationGoal;
     uint256 public totalDonations;
+    string public description;
+    uint256 public start;
+    uint256 public end;
+
+    //solidity has no decimal numbers, so we use these variables to calculate percentages
+    uint256 constant basepoints = 10000;
 
     mapping(address => uint256) public donations;
+    address[] public validators;
+    mapping(address => bool) public isValidator;
+
+    mapping(uint256 => Milestone) public milestones;
+    mapping(uint256 => mapping(address => bool)) public votesForMilestone;
+    mapping(uint256 => mapping(address => bool)) hasVoted;
+
+    struct Milestone{
+        uint16 percentage;
+        uint16 approvedCount;
+        uint16 rejectedCount;
+        bool paid;
+    }
+
+    uint256 public neededVoteMajorityInBps = 667;
 
     //events create log entries on blockchain
     //indexed address enables efficient search for donations from a specifc donor 
@@ -15,11 +37,30 @@ contract Donation{
         uint256 amount
     );
 
-    constructor(uint256 goal){
+    event VoteSubmitted(
+        address indexed validator,
+        uint256 milestoneIndex,
+        bool approved
+    );
+
+    constructor(uint256 goal, address[] memory validators_, string memory description_, uint256 duration, uint16[] memory milestonePercentages){
         donationGoal = goal;
+        validators = validators_;
+        for(uint i = 0; i < validators_.length; i++){
+            isValidator[validators_[i]] = true;
+        }
+        description = description_;
+        contractOwner = msg.sender;
+        start = block.timestamp;
+        end = block.timestamp + duration;
+
+        for(uint i = 0; i < milestonePercentages.length; i++){
+            milestones[i] = Milestone(milestonePercentages[i], 0, 0, false);
+        }
+
     } 
 
-    function donate() external payable isPositiveDonation(msg.value){
+    function donate() external payable isPositiveDonation(msg.value) isInTimeFrame(){
         uint256 donation = msg.value;
 
         totalDonations += donation;
@@ -28,12 +69,54 @@ contract Donation{
         emit DonationReceived(msg.sender, msg.value);
     }
 
+    function voteMilestone(uint milestoneIndex, bool vote) external isAllowedToVote{
+        require(!hasVoted[milestoneIndex][msg.sender]);
+        hasVoted[milestoneIndex][msg.sender] = true;
+        votesForMilestone[milestoneIndex][msg.sender] = vote;
+
+        emit VoteSubmitted(msg.sender, milestoneIndex, vote);
+    }
+
+    function payout(uint milestoneIndex) public isApproved(milestoneIndex){
+        require(!milestones[milestoneIndex].paid);
+        payable(contractOwner).transfer(address(this).balance);
+    }
+
     modifier isPositiveDonation(uint256 x){
         require(x > 0, "only positive values can be donated");
         _;
     }
 
+    modifier isInTimeFrame(){
+        require(block.timestamp >= start && block.timestamp <= end, "not in timeframe");
+        _;
+    }
+
+    modifier isAllowedToVote(){
+        require(isValidator[msg.sender] == true);
+        _;
+    }
+
+    modifier isApproved(uint256 milestoneIndex){
+        require(calculatePercentageInBps(milestones[milestoneIndex].approvedCount, validators.length) >=  neededVoteMajorityInBps);
+        _;
+    }
     function getContractBalance() external view returns (uint256){
         return address(this).balance;
+    }
+
+    function getValidator(uint256 x) external view returns (address){
+        return validators[x];
+    }
+
+    //no decimal numbers so we need basepoints
+    //basepoints is 100%
+    //if basepoints = 10000, then 1% = 100bps
+    function calculatePortion(uint256 value, uint256 bsp)internal pure returns (uint256) {
+        return (value * bsp) / basepoints;
+    }
+
+    function calculatePercentageInBps(uint256 value, uint256 total)internal pure returns (uint256) {
+        return (value * basepoints) / total;
     }
 }
--- packages/contracts/contracts/Donation.t.sol
+++ packages/contracts/contracts/Donation.t.sol
@@ -3,17 +3,37 @@ pragma solidity ^0.8.28;
 
 import {Donation} from "./Donation.sol";
 import {Test} from "forge-std/Test.sol";
+import{console2} from "forge-std/console2.sol";
 
 
 contract DonationTest is Test {
   Donation donation;
+  address[] validators; 
+  string description = "This is a description";
+  uint256 duration = 7 days;
 
   function setUp() public {
-    donation = new Donation(10 ether);
+    validators.push(makeAddr("a"));
+    validators.push(makeAddr("b"));
+    donation = new Donation(10 ether, validators, description, duration);
   }
 
   function test_InitialDonations() public view {
     require(donation.totalDonations() == 0, "Initial value should be 0");
+  }
+
+  function test_EndDate() public view {
+    assertEq(donation.start() + duration, donation.end());
+  }
+
+  function test_InitialValidators() public view{
+    for (uint i = 0; i < validators.length; i++){
+      require(donation.getValidator(i) == validators[i]);
+    }
+  }
+
+  function test_InitialDescription() public view {
+    assertEq(donation.description(), "This is a description");
   }
 
   function testFuzz_Donate(uint256 x) public {
@@ -22,8 +42,25 @@ contract DonationTest is Test {
     require(donation.totalDonations() == x);
   }
 
+  function testFuzz_DonationPerDonatee(address sender, uint256 x, uint256 y) public {
+    vm.assume(x > 0 && x < 10 ether);
+    vm.assume(y > 0 && y < 10 ether);
+    vm.assume(sender != address(0));
+    vm.deal(sender, x+y);
+    vm.startPrank(sender);
+    donation.donate{value: x}();
+    donation.donate{value: y}();
+    require(donation.donations(sender)== x+y);
+  }
+
   function test_DonateNothing() public {
     vm.expectRevert();
     donation.donate{value:0}();
+  }
+
+  function test_DonateAfteEndDate() public {
+    vm.warp(donation.end() + 1);
+    vm.expectRevert();
+    donation.donate{value: 5}();
   }
 }
